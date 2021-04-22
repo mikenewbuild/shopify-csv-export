33f79091faf486f06de85e8c9eb36e2a8aa5c39a
Mike Fallows <43390704+mikenewbuild@users.noreply.github.com>
2021-04-22 21:35:06 +0100

    Add export script

---
+++ main.js
@@ -0,0 +1,51 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const { parse } = require('json2csv');
+require('dotenv').config();
+
+const { API_KEY, PASSWORD, DOMAIN, RESOURCE, FIELDS } = process.env;
+const baseUrl = `https://${API_KEY}:${PASSWORD}@${DOMAIN}`;
+
+const resources = [];
+const filename = `${RESOURCE}.csv`;
+
+const outputCsv = (data, fields) => {
+  const csv = parse(data, { fields });
+  fs.writeFile(filename, csv, (err) => {
+    if (err) throw err;
+    console.log(`Done! saved as "${filename}"`);
+  });
+}
+
+const parseNextUrl = (link) => {
+  const next = link.split(',').find(i => i.includes('rel="next"'));
+  if (!next) return '';
+  const regex = new RegExp(/<(.+)>/g);
+  return regex.exec(next)[1].replace(`https://${DOMAIN}`, baseUrl);
+}
+
+const getResources = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  data[RESOURCE].forEach(row => resources.push(row));
+  console.log("Fetched......", resources.length);
+
+  const link = await response.headers.get('link') || '';
+  const nextUrl = parseNextUrl(link);
+
+  if (!nextUrl.startsWith('https')) {
+    console.log("\nPreparing CSV...");
+    outputCsv(resources, FIELDS.split(','));
+    return;
+  }
+
+  getResources(nextUrl);
+}
+
+console.log(`Fetching ${RESOURCE} (${FIELDS}) from ${DOMAIN}\n`);
+
+try {
+  getResources(`${baseUrl}/admin/${RESOURCE}.json?limit=250&fields=${FIELDS}`);
+} catch (error) {
+  console.error(error);
+}
